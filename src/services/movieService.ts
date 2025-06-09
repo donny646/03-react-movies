b14bd63ce59adabe03d8d4cd11b4339285d93e0d
Artem <artemkolesnik0201@gmail.com>
2025-06-09 18:47:15 +0100

    1

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -11,7 +11,9 @@ export default async function fetchMovies(query: string): Promise<Movie[]> {
   const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
 
   const options = {
-    headers: { Authorization: `Bearer ${myKey}` },
+    headers: {
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjIxNjNhMGJmODcwNTM0ZjdjMWRlMzg0ZDlmZDYxZCIsIm5iZiI6MTc0OTQ5MDY2Mi4xMjk5OTk5LCJzdWIiOiI2ODQ3MWJlNjdmM2RlZjZmODEzMDFiZTQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Pr0Cq4JlvRJfVOvsYJo6-CafmnjINgvR3m7t7A9w2-o`,
+    },
   };
 
   const res = await axios.get<Movies>(url, options);
